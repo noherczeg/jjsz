09b9d3aea602d74399490edab4d17680ccd26f38
Norbert Csaba Herczeg <noherczeg@gmail.com>
2013-04-16 16:52:24 -0700

    error uzenetek

--- index.php
+++ index.php
@@ -8,7 +8,20 @@ require 'init.php';
 try {
     System\Router::loadController();
 } catch (Exception $e) {
-    if ($e->getCode() == 404) {
-        echo '<h3>404 - A kért oldal nem található!</h3>';
+    switch ($e->getCode()) {
+        
+        // Statusz kodokrol bovebben: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+        case 404:
+            echo '<h3>404</h3><p>A kért oldal nem található!</p>';
+            break;
+        case 401:
+            echo '<h3>401</h3><p>Authentikáció sikertelen!</p>';
+            break;
+        case 403:
+            echo '<h3>403</h3><p>Hozzáférés megtagadva!</p>';
+            break;
+        default:
+            echo '<h3>500</h3><p>Fogalmunk sincs mi a baj o.O</p>';
+            break;
     }
 }
